1e98fa5cce35b9eea0d2bcfc62ccccd1bf7e51ca
Prakash-nandaniya <b22ee050@iitj.ac.in>
2023-12-15 19:13:37 +0530

    add files

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <fstream>
+#include <sstream>
+#include <map>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+}
